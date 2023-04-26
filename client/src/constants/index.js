bba38ddbb3d837a0001679fbdbf27fb8f1a31004
liam <liam_killingback@yahoo.com>
2023-04-26 20:47:09 +1000

    final build mk2

--- client/src/constants/index.js
+++ client/src/constants/index.js
@@ -6,14 +6,3 @@ export const genres = [
     "Gaming",
     "Music",
 ]
-
-export const blogPosts = [
-    {
-        title: "React-redux",
-        description: "Using react-redux alongsite redux-persist to achieve state shared across all pages/components",
-        content: "",
-        publishedAt: "23rd April, 2023",
-        urlToImage: "https://waftengine.org/public/blog/1B5EE4D5D773F8A-RR.jpg",
-        
-    }
-]
